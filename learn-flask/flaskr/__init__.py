7a0d5d2e3494f4a23c1b121801ce0e8040c12662
phucnn <phucnn@vng.com.vn>
2022-04-16 14:55:06 +0700

    day 7 - flask

--- learn-flask/flaskr/__init__.py
+++ learn-flask/flaskr/__init__.py
@@ -1,11 +1,13 @@
 import os
 
 from flask import Flask
+import flask
+
 
 def create_app(test_config=None):
     print("ok")
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_folder="static/")
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -27,6 +29,8 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    app.logger.error("static " + app._static_folder)
 
     # a simple page that says hello
     @app.route('/hello')
